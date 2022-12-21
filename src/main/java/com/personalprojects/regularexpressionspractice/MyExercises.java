1b0369aa6f82798becf489e3153d9900e0ade4a1
arubla <arunblatchley@gmail.com>
2022-12-20 19:22:16 -0500

    3 questions written

--- src/main/java/com/personalprojects/regularexpressionspractice/MyExercises.java
+++ src/main/java/com/personalprojects/regularexpressionspractice/MyExercises.java
@@ -2,15 +2,33 @@ package com.personalprojects.regularexpressionspractice;
 
 public class MyExercises {
 
-    //given a string, return a string that removes all non-letter characters and replaces them with spaces.
+    //1. Given a string, return a string that removes all non-letter characters and replaces them with spaces.
 
     public String splitNotLetter(String str) {
 
-        String temp = str.replaceAll("[^a-zA-Z]+", " ");
+        return str.replaceAll("[^a-zA-Z]+", " ");
 
-        return temp;
     }
 
+    //2. Given a credit card number as a string, return a string that eliminates all special characters
+    // that might be dividing the numbers.
+
+    public String normalizeNum(String str) {
+        return str.replaceAll("[^0-9]", "");
+    }
+
+    //3. Given a credit card number as a string, return a string that replaces the first 12 digits with "X"
+    //and changes all special characters to "-"
+
+    public String maskNum(String cc) {
+        return cc.replaceAll("[^0-9]", "-").replaceAll("\\d{4}-\\d{4}-\\d{4}", "XXXX-XXXX-XXXX");
+
+    }
+
+    public static void main(String[] args) {
+        MyExercises myExercises = new MyExercises();
+        System.out.println(myExercises.maskNum("1234!5678-9012-3456"));
+    }
 
 
 }
--- src/test/java/MyExercisesTest.java
+++ src/test/java/MyExercisesTest.java
@@ -21,4 +21,30 @@ public class MyExercisesTest {
 
     }
 
+    @Test
+    public void normalizeNum() {
+        String ccDash = "1234-5678-9012-3456";
+        String ccStar = "1234*5678*9012*3456";
+        String ccSpace = "1234 5678 9012 3456";
+
+        Assertions.assertEquals("1234567890123456", myExercises.normalizeNum(ccDash));
+        Assertions.assertEquals("1234567890123456", myExercises.normalizeNum(ccStar));
+        Assertions.assertEquals("1234567890123456", myExercises.normalizeNum(ccSpace));
+    }
+
+    @Test
+    public void maskNum() {
+        String ccDash = "1234-5678-9012-3456";
+        String ccStar = "1234*5678*9012*3456";
+        String ccSpace = "1234 5678 9012 3456";
+        String ccMix = "1234!5678&9012*3456";
+        String expected = "XXXX-XXXX-XXXX-3456";
+
+        Assertions.assertEquals(expected, myExercises.maskNum(ccDash));
+        Assertions.assertEquals(expected, myExercises.maskNum(ccStar));
+        Assertions.assertEquals(expected, myExercises.maskNum(ccSpace));
+        Assertions.assertEquals(expected, myExercises.maskNum(ccMix));
+
+    }
+
 }
